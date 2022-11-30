45ff2d33a2bf8052f9c81200a35ee1faad93add4
Michal Bielecki <michalpatrykbielecki@gmail.com>
2022-11-30 14:17:30 +0100

    opinions-carousel with exaple img

--- src/components/Opinions/Opinions.js
+++ src/components/Opinions/Opinions.js
@@ -5,42 +5,28 @@ import styles from './Opinions.module.scss';
 const Opinions = () => {
     return (
         <Carousel className={styles.carousel}>
-          <Carousel.Item>
+          <Carousel.Item className={styles.item}>
             <img
-              className="d-block w-100"
-              src="holder.js/800x400?text=First slide&bg=373940"
+              className={styles.img}
+              src="https://i.postimg.cc/Dw39KDRh/pizza-3.jpg"
               alt="Opinia 1"
             />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
           </Carousel.Item>
-          <Carousel.Item>
+
+          <Carousel.Item className={styles.item}>
             <img
-              className="d-block w-100"
-              src="holder.js/800x400?text=Second slide&bg=282c34"
+              className={styles.img}
+              src="https://i.postimg.cc/Dw39KDRh/pizza-3.jpg"
               alt="Opinia 2"
             />
-    
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
           </Carousel.Item>
-          <Carousel.Item>
+          
+          <Carousel.Item className={styles.item}>
             <img
-              className="d-block w-100"
-              src="holder.js/800x400?text=Third slide&bg=20232a"
+              className={styles.img}
+              src="https://i.postimg.cc/Dw39KDRh/pizza-3.jpg"
               alt="Opinia 3"
             />
-    
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
       );
