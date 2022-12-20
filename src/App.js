73944cb75ba4cabd8238c9ac9f5e28d86a127822
Michal Bielecki <michalpatrykbielecki@gmail.com>
2022-12-20 18:57:26 +0100

    home on homepage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,33 @@
 import styles from "./App.module.scss";
+//import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
+//import { useEffect, useState } from 'react';
+//import CircleCursor from './components/CircleCursor/CircleCursor.js';
+import Nav from './components/Nav/Nav';
+import ColumnList from "./components/MessagesColumnList/ColumnList.js";
+import { Routes, Route } from 'react-router-dom';
+import Home from "./components/Home/Home.js";
+import AboutUs from './components/AboutUs/AboutUs.js';
+//import { useState } from "react";
+
+const App = () => {
+
+  return (
+  <main className={styles.body}>
+    <Nav /> 
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/mess" element={<ColumnList />} />
+      <Route path="/about" element={<AboutUs />} />
+    </Routes>
+  </main>
+  );
+};
+
+export default App;
+
+/*
+import styles from "./App.module.scss";
 import Nav from './components/Nav/Nav';
 import ColumnList from "./components/MessagesColumnList/ColumnList.js";
 import Home from "./components/Home/Home.js";
@@ -21,34 +50,6 @@ const App = () => {
       {currentScreen === SCREEN_NAMES.HOME && <Home />}
       {currentScreen === SCREEN_NAMES.MESS && <ColumnList />}
       {currentScreen === SCREEN_NAMES.ABOUT && <AboutUs />}
-  </main>
-  );
-};
-
-export default App;
-
-/*
-import styles from "./App.module.scss";
-//import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
-//import { useEffect, useState } from 'react';
-//import CircleCursor from './components/CircleCursor/CircleCursor.js';
-import Nav from './components/Nav/Nav';
-import ColumnList from "./components/MessagesColumnList/ColumnList.js";
-import { Routes, Route } from 'react-router-dom';
-import Home from "./components/Home/Home.js";
-import AboutUs from './components/AboutUs/AboutUs.js';
-import { useState } from "react";
-
-const App = () => {
-
-  return (
-  <main className={styles.body}>
-    <Nav /> 
-    <Routes>
-      <Route path="/home" element={<Home />} />
-      <Route path="/mess" element={<ColumnList />} />
-      <Route path="/about" element={<AboutUs />} />
-    </Routes>
   </main>
   );
 };
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,4 +1,19 @@
 import styles from './Nav.module.scss';
+
+const Nav = () => {
+    return (
+        <main className={styles.main}>
+            <a href="/mess" className={styles.mess}>MESS</a>
+            <a href="/home" className={styles.home}>HOME</a>
+            <a href="/about" className={styles.about}>O NAS</a>
+        </main>
+    
+    )
+}
+
+export default Nav;
+/*
+import styles from './Nav.module.scss';
 import { SCREEN_NAMES } from '../../App';
 
 const Nav = ({selectScreen}) => {
@@ -24,22 +39,6 @@ const Nav = ({selectScreen}) => {
                     e.preventDefault();
                     selectScreen(SCREEN_NAMES.ABOUT)
                 }}>O NAS</a>
-        </main>
-    
-    )
-}
-
-export default Nav;
-
-/*
-import styles from './Nav.module.scss';
-
-const Nav = () => {
-    return (
-        <main className={styles.main}>
-            <a href="/mess" className={styles.mess}>MESS</a>
-            <a href="/home" className={styles.home}>HOME</a>
-            <a href="/about" className={styles.about}>O NAS</a>
         </main>
     
     )
